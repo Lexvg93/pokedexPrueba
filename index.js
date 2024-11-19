74df07f95de178e2780fbdc41efc15e92f337338
Alejandro <alejandro_gonzalo92@icloud.com>
2024-11-19 16:01:06 +0100

    subida 5
    se añade funcion cargaPokemon

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const listaPokemon = document.querySelector('#listaPokemon');
 const botonesHeader = document.querySelector('.btn-header');
 let pokemons = [];
 let url = 'https://pokeapi.co/api/v2/pokemon/';
+
+const cargaPokemon = async()=>{
+    for (let i=1; i<=151;i++){
+        const response = await fetch(url+`${i}`);
+        if(response.ok){
+            const jsResponse = await response.json();
+            pokemons.push(jsResponse);
+            
+        }
+    }
+}
