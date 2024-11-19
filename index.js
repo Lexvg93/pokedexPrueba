709c96c7c9f3f7a5526a0eff28e33197627d562b
Alejandro <alejandro_gonzalo92@icloud.com>
2024-11-19 15:49:34 +0100

    subida 6
    se añade imagen pokemon y codigo de funcion cargaPokemon

---
+++ index.js
@@ -0,0 +1,4 @@
+const listaPokemon = document.querySelector('#listaPokemon');
+const botonesHeader = document.querySelector('.btn-header');
+let pokemons = [];
+let url = 'https://pokeapi.co/api/v2/pokemon/';
